9d4b525dad60ffa57864c8b9d0b693a614952185
damkenny <adeniyekehinde0@gmail.com>
2022-05-19 16:15:32 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+async function start() {
+  const app = express();
+  const port = 3000;
+
+  app.get('/health', (req, res) => res.status(200));
+
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+}
+
+(async () => {
+  await start();
+})();
