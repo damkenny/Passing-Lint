7f17dd4f9eb838d476a23023850b79258913edec
damkenny <adeniyekehinde0@gmail.com>
2022-05-19 16:43:12 +0100

    prettier

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require('express')
 
 async function start() {
-  const app = express();
-  const port = 3000;
+    const app = express()
+    const port = 3000
 
-  app.get('/health', (req, res) => res.status(200));
+    app.get('/health', (req, res) => res.status(200))
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
 }
 
-(async () => {
-  await start();
-})();
+;(async () => {
+    await start()
+})()
